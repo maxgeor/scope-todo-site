83c3723048e008302ea902c006ef27898d460a30
Max Georgopoulos <maxgeorgopoulos@Maxs-MacBook-Air.local>
2021-12-04 17:21:56 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      colors: {
+        green: {
+          light: '#12C8A2',
+          DEFAULT: '#00b691',
+        },
+        black: '#101010',
+      },
+      boxShadow: {
+        widget: '0px 4.545454978942871px 13.636364936828613px 0px #00000017;',
+      },
+      dropShadow: {
+        float: '0px 0.9215261936187744px 7.372209548950195px 0px #00000021; 0px 3.6861047744750977px 10px 7.372209548950195px #00000006;',
+      },
+      maxWidth: {
+        '4.5xl': '60rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
